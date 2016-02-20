80307da064651b7431d1c6feedd39c0df536448a
minamorl <minamorl@users.noreply.github.com>
2016-02-21 03:31:51 +0900

    workaround for gunicorn (this is ugly, but works fine)

--- mead/server.py
+++ mead/server.py
@@ -11,13 +11,19 @@ class Mead():
         self.app = aiohttp.web.Application()
         self.router = router
         self.session_encrypt_key = session_encrypt_key
+        self.prepared = False
+
+    def prepare(self):
+        if not self.prepared:
+            for method, path, obj in self.router:
+                self.app.router.add_route(method, path, obj)
+            aiohttp_session.setup(self.app, aiohttp_session.cookie_storage.EncryptedCookieStorage(self.session_encrypt_key))
+        self.prepared = True
 
     async def init(self, loop, address, port):
         handler = self.app.make_handler()
-        for method, path, obj in self.router:
-            self.app.router.add_route(method, path, obj)
+        self.prepare()
         srv = await loop.create_server(handler, address, port)
-        aiohttp_session.setup(self.app, aiohttp_session.cookie_storage.EncryptedCookieStorage(self.session_encrypt_key))
         return srv, handler
 
     def serve(self, port=8080, address="127.0.0.1"):
@@ -30,3 +36,10 @@ class Mead():
             loop.run_forever()
         except KeyboardInterrupt:
             loop.run_until_complete(handler.finish_connections())
+
+    def __call__(self):
+        return self
+
+    def __getattr__(self, attr):
+        self.prepare()
+        return self.app.__getattribute__(attr)
