da5aadbe5ca86191e5286bb07a4e35186d8362d7
minamorl <minamorl@users.noreply.github.com>
2016-02-23 00:23:47 +0900

    pep8 style

--- mead/context.py
+++ mead/context.py
@@ -1,11 +1,13 @@
 import aiohttp_session
+
+
 class Context(dict):
     pass
 
 async def create_context(request):
     session = await aiohttp_session.get_session(request)
     return Context({
-        "params":  await request.post(),
+        "params": await request.post(),
         "query": request.GET,
         "session": session,
     })
