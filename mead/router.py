83babcf06bd5f18f8321828c58803529243e07ea
minamorl <minamorl@users.noreply.github.com>
2016-02-23 00:20:35 +0900

    add validation on router

--- mead/router.py
+++ mead/router.py
@@ -1,14 +1,24 @@
 import functools
+import collections
 from .context import create_context
 
 
+Route = collections.namedtuple("Route", ["method", "path", "object"])
+
+
 class Router(list):
+
+    def append(self, obj):
+        if not isinstance(obj, Route):
+            raise RoutingException
+        super(Router, self).append()
 
     def route(self, path, methods=["GET"]):
         def wrapper(func):
             @functools.wraps(func)
             def _wrapper(*args, **kwargs):
                 return func(*args, **kwargs)
+
             _methods = methods
             if not isinstance(methods, list):
                 _methods = [methods]
@@ -22,4 +32,4 @@ class Router(list):
             context = await create_context(request)
             return await obj(context)
 
-        self.append((method, path, wrapped))
+        self.append(Route(method, path, wrapped))
