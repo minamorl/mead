ef8fe6cd9a1aa7acca0e16f886229b027ef54dbe
minamorl <minamorl@users.noreply.github.com>
2016-02-23 13:52:07 +0900

    add advance usage

--- examples/runserver.py
+++ examples/runserver.py
@@ -1,14 +1,45 @@
 from mead import Mead, response, Router
 from mead.objects import JSONObject
 
+
 router = Router()
 mead = Mead(router=router)
+
 
 @router.route("/", methods=["GET"])
 async def helloworld(context):
     return response(JSONObject({
-        "results":"Hello"
+        "results": "Hello, World!"
     }))
+
+
+@router.route("/greeting", methods=["GET"])
+async def greeting(context):
+    ses = await context.session
+    try:
+        username = ses["username"]
+        return response(JSONObject({
+            "results": "Hello, {}".format(username)
+        }))
+    except KeyError:
+        return response(JSONObject({
+            "results": "Hello."
+        }))
+
+
+@router.route("/greeting", methods=["POST"])
+async def greeting(context):
+    ses = await context.session
+    params = await context.params
+    try:
+        ses["username"] = params["username"]
+        return response(JSONObject({
+            "results": "ok"
+        }))
+    except KeyError:
+        return response(JSONObject({
+            "results": "Something went wrong."
+        }))
 
 if __name__ == '__main__':
     mead.serve()
