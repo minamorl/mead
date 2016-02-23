579109f29296ade9b6e304e67bf57ac90e9e5440
minamorl <minamorl@users.noreply.github.com>
2016-02-23 13:55:58 +0900

    Bump version: 0.0.9 → 0.0.10

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ class Tox(TestCommand):
 
 setup(
     name="mead",
-    version="0.0.9",
+    version="0.0.10",
     packages=find_packages(),
     install_requires = [],
     author = "minamorl",
